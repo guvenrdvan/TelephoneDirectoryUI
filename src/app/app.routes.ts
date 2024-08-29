a54bff0b60f7421ee05b6d9929051a5118eb6ff9
guvnrdvan <guvnrdvan@gmail.com>
2024-08-29 11:01:36 +0300

    updateProject

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './Components/user/user.component';
+import { Component } from '@angular/core';
+import { LoginComponent } from './pages/auth/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import path from 'path';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ɵBrowserGetTestability } from '@angular/platform-browser';
+import { ReadVarExpr } from '@angular/compiler';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import { authGuard } from './guards/auth-guard.guard';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+
+    { path: '', redirectTo: 'login', pathMatch: 'full', },
+    { path: 'login', component: LoginComponent, canActivate: [authGuard] },
+    { path: 'register', component: RegisterComponent },
+    { path: ' user', component: UserComponent, },
+
+    {
+        path: '',
+        component: UserComponent,
+        children: [
+            {
+                path: 'user',
+                component: UserComponent,
+                canActivate: [authGuard]
+            }
+        ]
+    },
+];
